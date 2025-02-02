d16d0b859c33517553c534292a342ebaafcc5151
Angkasit Karoonchart <108455691+Marguro@users.noreply.github.com>
2025-02-03 00:31:13 +0700

    feat: create AlarmNavigator reference from WorldClockNavigator

---
+++ navigator/AlarmNavigator.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createStackNavigator, TransitionSpecs, CardStyleInterpolators } from '@react-navigation/stack';
+import Alarm from '../screens/Alarm';
+import AddAlarm from '../screens/AddAlarm';
+
+const Stack = createStackNavigator();
+
+export default function AlarmNavigator() {
+    return (
+        <Stack.Navigator
+            initialRouteName="Alarm"
+            screenOptions={{
+                headerShown: false,
+                transitionSpec: {
+                    open: TransitionSpecs.TransitionIOSSpec,
+                    close: TransitionSpecs.TransitionIOSSpec,
+                },
+                cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+            }}
+        >
+            <Stack.Screen name="Alarm" component={Alarm} options={{ title: 'Alarm' }} />
+            <Stack.Screen name="AddAlarm" component={AddAlarm} options={{ title: 'Add Alarm' }} />
+        </Stack.Navigator>
+    );
+}
